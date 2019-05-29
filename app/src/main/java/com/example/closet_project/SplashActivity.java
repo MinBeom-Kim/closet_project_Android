472418e0617b1c728c16d0cd48d8ebb8923c3ed3
mbk <kmb3755@gmail.com>
2019-05-30 00:20:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/closet_project/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.closet_project;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+public class SplashActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(1300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+}
